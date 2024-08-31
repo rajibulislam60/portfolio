6f5fedf720759e4a0f349f065c8110da08196122
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-08-31 16:19:11 +0600

    add text type animation

--- src/pages/Bannar.jsx
+++ src/pages/Bannar.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
-import BannarImage from '../assets/BannarPic.png';
+import BannarImage from '../assets/bannar.png';
 import Container from '../Components/Container';
 import Button from '../Components/Button';
+import { TypeAnimation } from "react-type-animation";
 
 const Bannar = () => {
 
   return (
-    <div className="h-screen flex bg-slate-400">
+    <div className="mt-[98px]">
       <Container>
-        <div className=" h-full flex justify-between items-center object-cover">
+        <div className=" flex justify-between items-center ">
           <div>
             <h3 className="uppercase font-normal text-[20px]">
               I am mern stack developer
             </h3>
-            <h1 className="text-[60px] leading-1.7 font-bold mt-[40px]">
-              Creative Design <br />
-              and Web Develop
+            <h1 className="w-[700px] text-[60px] leading-1.7 font-bold mt-[40px]">
+              Creative Design and {" "}
+              <div className='text-orange-400'>
+                <TypeAnimation
+                  sequence={["Web Develop", 1000, "Front-End Develop", 1000, "Back-End Develop", 1000, "Full Stack Develop", 1000]}
+                  style={{ fontSize: "60px" }}
+                  repeat={Infinity}
+                />
+              </div>
             </h1>
             <p className="text-[18px] leading-1.7 font-light mt-[20px] max-w-[663px]">
               Nam libero tempore, cum soluta nobis est eligendi optio cumque
@@ -23,7 +30,7 @@ const Bannar = () => {
               omnis voluptas assumenda est, omnis dolor repellendus. Temporibus
               autem quibusdam et.
             </p>
-            <div className='mt-[50px]'>
+            <div className="mt-[50px]">
               <Button
                 className="bg-orange-600 text-white px-5 py-3 text-[21px] font-medium rounded-md hover:bg-black duration-[0.4s] max-w-[228px] mt-[50px]"
                 name="Download My CV"
@@ -31,8 +38,8 @@ const Bannar = () => {
               />
             </div>
           </div>
-          <div>
-            <img src={BannarImage} alt="Profile Picture" />
+          <div className=''>
+            <img src={BannarImage} alt="Profile Picture" className='h-[800px] w-[600px] object-cover'/>
           </div>
         </div>
       </Container>
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -11,7 +11,7 @@ const Service = () => {
           <div>
             <Title titleName="My Service" titleItem="Services I offer" />
           </div>
-          <div className="mt-[70px] flex justify-center gap-8">
+          <div className="mt-[70px] flex justify-center gap-8 flex-wrap">
             <div className="border border-orange-100 max-w-[350px] text-center px-[30px] py-10 hover:bg-orange-100 rounded-md">
               <VscVmConnect className="text-[60px] mx-auto text-orange-300" />
               <h3 className="text-[24px] leading-1.7 font-semibold mt-4 mb-3">
@@ -25,7 +25,27 @@ const Service = () => {
             <div className="border border-orange-100 max-w-[350px] text-center px-[30px] py-10 hover:bg-orange-100 rounded-md">
               <VscVmConnect className="text-[60px] mx-auto text-orange-300" />
               <h3 className="text-[24px] leading-1.7 font-semibold mt-4 mb-3">
+                Back-End Developer
+              </h3>
+              <p className="text-[16px] leading-1.7 font-light text-slate-500">
+                Hen our power of choice is untrammelled and when nothing
+                prevents our being able
+              </p>
+            </div>
+            <div className="border border-orange-100 max-w-[350px] text-center px-[30px] py-10 hover:bg-orange-100 rounded-md">
+              <VscVmConnect className="text-[60px] mx-auto text-orange-300" />
+              <h3 className="text-[24px] leading-1.7 font-semibold mt-4 mb-3">
                 Web Developer
+              </h3>
+              <p className="text-[16px] leading-1.7 font-light text-slate-500">
+                Hen our power of choice is untrammelled and when nothing
+                prevents our being able
+              </p>
+            </div>
+            <div className="border border-orange-100 max-w-[350px] text-center px-[30px] py-10 hover:bg-orange-100 rounded-md">
+              <VscVmConnect className="text-[60px] mx-auto text-orange-300" />
+              <h3 className="text-[24px] leading-1.7 font-semibold mt-4 mb-3">
+                Full Stack Developer
               </h3>
               <p className="text-[16px] leading-1.7 font-light text-slate-500">
                 Hen our power of choice is untrammelled and when nothing
